2ddb0e3016b1f405312ef2fbe42054a2eb2d481e
ashutosh708 <85440783+ashutosh708@users.noreply.github.com>
2021-06-07 10:02:36 +0530

    Add files via upload

---
+++ geography chapter India-Size and Location.py
@@ -0,0 +1,195 @@
+print("welcome to geography chapter 'India-Size and Location'")
+print("NOTE 1:- DO NOT WRITE ANY TYPE SIGNS e.g.-QUESTION MARK/FULL STOP etc.")
+print("NOTE 2:-WRITE QUESTION IN THE FORM: e.g-what is rest")
+print("")
+print("")
+b=True
+
+while b==True:
+
+    a=input("enter your question related to this chapter at class 9 level").lower()
+    print("")
+    if a=='what is latitude':
+        print("")
+        print("Latitude is a geographic coordinate that specifies thye North-South position of a point on Earth's Surface")
+
+    elif a=='name important latitudes':
+        print("")
+        print("1.Equator-It is 0 degree latitude.It is the latitude which divides the Earth in two equal hemispheres")
+        print("")
+        print("2.Tropic of cancer-It is 23.5 degree north latitude which divides India in two equal parts.")
+        print("")
+        print("3.Tropic of Capricorn-It is 23.5 degree SOUTH latitude")
+        print("")
+        print("4.Arctic Circle-It is 66.5 degree NORTH latitude")
+        print("")
+        print("5.Antartic Circle-It is 66.5 degree SOUTH latitude")
+
+
+    elif a=='what is equator':
+        print("")
+        print("It is 0 degree latitude.It is the latitude which divides the Earth in two equal hemispheres")
+        print("The two hemispheres are")
+        print("")
+        print("1.Northern Hemisphere")
+        print("2.southern Hemishpheres")
+
+    elif a=='what is tropic of cancer':
+        print("")
+        print("It is 23.5 degree north latitude which divides India in two equal parts.")
+
+    elif a=='what is tropic of capricorn':
+       print("")
+       print("It is 66.5 degree SOUTH latitude")
+        
+
+    elif a=='what is arctic circle':
+        print("")
+        print("It is 66.5 degree NORTH latitude")
+
+    elif a=='what is antarctic circle':
+        print("")
+        print("It is 66.5 degree SOUTH latitude")
+
+    elif a=='what is longitude':
+        print("")
+        print("The imaginary lines that pass from North Pole to South Pole are called longitude")
+
+    elif a=='name some important longitudes':
+        print("")
+        print("1.Prime Meridian (0 degree)")
+        print("2.International Date Line (180 degree)")
+
+    elif a=='what is prime meridian':
+        print("")
+        print("The zero degree longitude is called prime meridian.It is also called Greenwich Meridian Time(GMT)")
+
+    elif a=='what is international date line':
+        print("")
+        print("It is the imaginary line which is 180 degree apart from prime meridian.")
+        print("")
+        print("IDL seperates days of week in western and eastern hemisphere.")
+        print("")
+        print("Time increases when we go from Eastern to Western Hemisphere")
+
+    elif a=='what is the location of india':
+        print("India lies in the North-Eastern Hemisphere in between ")
+        
+    elif a=='what is the latitudinal extent of india':
+        print("")
+        print("The latitudinal extent of India is 8 degree 4 minute north to 37 degree 6 minute north")
+
+    elif a=='what is the longitudinal extent of india':
+        print("")
+        print("The longitudinal extent of India is 68 degree 7 minute east to 97 degree 25 minute east")
+
+    elif a=='what is the latitudinal and longitudinal extent of india':
+        print("")
+        print("The latitudinal extent of India is 8 degree 4 minute north to 37 degree 6 minute north")
+        print("")
+        print("The longitudinal extent of India is 68 degree 7 minute east to 97 degree 25 minute east")
+
+    elif a=='what is the land area of india':
+        print("")
+        print("3,287,263 sq.km")
+
+    elif a=='which countries are larger than india':
+        print("")
+        print("1.Russia")
+        print("2.Canada")
+        print("3.USA")
+        print("4.China")
+        print("5.Brazil")
+        print("6.Australia")
+
+    elif a=='what is the length of land boundary of india':
+        print("")
+        print("15200 km")
+        
+    elif a=='what is length of coastline of india':
+        print("")
+        print("7516.6 km")
+
+    elif a=='what is the distance between northernmost and southernmost point of india':
+        print("")
+        print("3214 km")
+
+    elif a=='what is the distance between westernmost and eastermost point of india':
+        print("")
+        print("2933 km")
+
+    elif a=='how many countries share its land boundary with india':
+        print("")
+        print("India share its land boundary with 9 contries")
+        print("")
+        print("1.Maldives")
+        print("2.Sri Lanka")
+        print("3.bangladesh")
+        print("4.Pakistan")
+        print("5. China")
+        print("6.Nepal")
+        print("7.Myanmar")
+        print("8.Bhutan")
+        print("9.Afhanistan")
+
+    
+    elif a=='name the countries which share its land boundary with india':
+        print("")
+        print("1.Maldives")
+        print("2.Sri Lanka")
+        print("3.bangladesh")
+        print("4.Pakistan")
+        print("5. China")
+        print("6.Nepal")
+        print("7.Myanmar")
+        print("8.Bhutan")
+        print("9.Afhanistan")
+
+    elif a=='from how many states tropic of cancer passes in india':
+        print("")
+        print("Tropic of Cancer passes from 8 states")
+        print("1.Rajasthan")
+        print("2.Chattisgarh")
+        print("3.Maharastra")
+        print("4.Gujarat")
+        print("5.Mizoram")
+        print("6.Tripura")
+        print("7.Jharkhand")
+        print("8.West Bengal")
+        
+
+    elif a=='name the states from which tropic of cancer passes':
+        print("")
+        print("1.Rajasthan")
+        print("2.Chattisgarh")
+        print("3.Maharastra")
+        print("4.Gujarat")
+        print("5.Mizoram")
+        print("6.Tripura")
+        print("7.Jharkhand")
+        print("8.West Bengal")
+
+    elif a=='how many latitudes are there in world':
+        print("")
+        print("181")
+
+    elif a=='how many longitutdes are there in world':
+        print("")
+        print("360")
+
+    elif a=='what is indian standard time':
+        print("")
+        print("It is a longitude which passes at 82.5 degree from Mirzapur. It is taken as the indian Standard Time because it is centrally located")
+        print("from east-west and from north-south")
+    else:
+        print("try again!! result not found")
+
+    print("")
+    print("")
+
+    c=input("do you want to repeat the program:yes/no")
+    if c=="yes":
+        continue
+    else:
+        exit()
+              
